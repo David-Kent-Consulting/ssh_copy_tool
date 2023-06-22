5117d276948afe7dfd23ad2493c3c6c2305265b6
hankwojteczkodki <hankwojteczko@davidkentconsulting.com>
2023-06-22 12:06:44 -0500

    First Commit

---
+++ ssh_copy_files.py
@@ -0,0 +1,35 @@
+#/bin/python3
+
+'''
+
+File name:      ssh_copy_tool.py
+
+Version:        A.1.0
+
+Purpose:        This utility's purpose is to copy files from a source Linux
+                VM to a target Linux VM using the python pysftp library.
+
+Requirements:   Please see the README.md file for requirements, caveats, and
+                use cases within the GIT repository
+                
+
+Author:         Hank L Wojteczko
+                Practice Manager
+                Kent Cloud Solutions, Inc
+                hankwojteczko@davidkentconsulting.com
+
+Created On:     22-June-2023
+
+Last Modified:  n/a
+
+License:        This code is considered part of the public domain and is considered
+                open source. The code is provided for the purpose of demonstrating
+                a practical use of the pysftp tool. The code is provided without
+                guarantee. You may use this code in part or in whole as you see
+                fit. You may modidify the code.
+
+                You agree that use of this code is solely at your own risk.
+
+
+
+'''
